95caa160cd24655ee82b2aaef740712f00baec4f
tom.gau <droopinwinter@gmail.com>
2024-05-15 08:31:19 +0800

    //進出場模擬交易紀錄原先只能做多，這裡增加做空功能 //另外列印交易結果時能顯示交易品種 updat file : Trade.py

--- Trade_11305132.py
+++ Trade_11305132.py
@@ -1,19 +1,19 @@
 #Trade
 
-def TotProfit(xapka, Ticker):
+def TotProfit(xapka):
 
     def ToBuy(i):
-        if   xapka.at[i,"xcmb"] >=  5:
+        if   xapka.at[i,"xcmb"] >=  6:
             return 1
-        elif xapka.at[i,"xcmb"] <= -4:
-            return -1               
+        elif xapka.at[i,"xcmb"] <= -6:
+            return -1
         else:
             return 0
         
     def ToSell(i):
-        if   xapka.at[i,"xcmb"] <= -1 and xapka.at[i-1,"xcmb"] > -1:
+        if   xapka.at[i,"xcmb"] == 0 and xapka.at[i-1,"xcmb"] > 0:
             return 1
-        elif xapka.at[i,"xcmb"] >= 1 and xapka.at[i-1,"xcmb"] < 1:
+        elif xapka.at[i,"xcmb"] == 0 and xapka.at[i-1,"xcmb"] < 0:
             return -1
         else:
             return 0
@@ -21,36 +21,34 @@ def TotProfit(xapka, Ticker):
     buyPrice = 0.0
     BuySellway =''
     EndDate = len(xapka)
-    #print(xapka.head())
+
     for i in range(1,len(xapka)):
-        if ToBuy(i) ==1 and xapka.at[i-1,"buy"] == 0:
+        if ToBuy(i) ==1:
             xapka.at[i,"buy"] =1
             buyPrice = xapka.at[i,"close"]
             BuySellway = BuySellway +'+Buy_'+str(xapka.at[i, "date"])[5:10]+' => ' 
-        elif ToBuy(i) ==-1 and xapka.at[i-1,"buy"] == 0:
+        elif ToBuy(i) ==-1:
             xapka.at[i,"buy"] =-1
             buyPrice = xapka.at[i,"close"]
             BuySellway = BuySellway +'-Buy_'+str(xapka.at[i, "date"])[5:10]+' => '             
-        elif ToSell(i)==1 and xapka.at[i-1,"buy"] == 1:
-            #xapka.at[i,"Sell"] =1
-            xapka.at[i,"buy"] =0
+        elif ToSell(i)==1:
+            xapka.at[i,"Sell"] =1
             xapka.at[i,"profit"] = (xapka.at[i,"close"] - buyPrice)/buyPrice
             print(BuySellway +'Sell_'+str(xapka.at[i, "date"])[5:10]+'_profit= '+str( xapka.at[i,"profit"].round(3)))
             BuySellway =''
-        elif ToSell(i)==-1 and xapka.at[i-1,"buy"] == -1:
-            #xapka.at[i,"Sell"] =-1
-            xapka.at[i,"buy"] =0
+        elif ToSell(i)==-1:
+            xapka.at[i,"Sell"] =-1
             xapka.at[i,"profit"] = (buyPrice - xapka.at[i,"close"])/buyPrice
             print(BuySellway +'Sell_'+str(xapka.at[i, "date"])[5:10]+'_profit= '+str( xapka.at[i,"profit"].round(3)))
             BuySellway =''
-        else:
-            #xapka.at[i,"Sell"] = xapka.at[i-1,"Sell"]
-            xapka.at[i,"buy"]  = xapka.at[i-1,"buy"]
 
     total =1000.0
     count = 0
     for i in range(1,EndDate):
         if xapka.at[i,"profit"] != 0:
             count = count +1
-            total = total + total*xapka.at[i,"profit"]
-    print( Ticker, "From [" + str(xapka.at[1,"date"]) +"] to ["+ str(xapka.at[len(xapka)-1,"date"]) +"] total =1000.0 after count: ["+str(count)+"] times total profit = "+str(round(total,3)) )
+            if xapka.at[i,"Sell"] ==1:
+                total = total + total*xapka.at[i,"profit"]
+            elif xapka.at[i,"Sell"] ==-1:
+                total = total - total*xapka.at[i,"profit"]
+    print("From [" + str(xapka.at[1,"date"]) +"] to ["+ str(xapka.at[len(xapka)-1,"date"]) +"] total =1000.0 after count: []"+str(count)+"] times total profit = "+str(round(total,3)) )
