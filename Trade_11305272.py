47dc7a6384b25c4a37730603a3ba98d6ea6f7a96
tom.gau <droopinwinter@gmail.com>
2024-05-29 08:10:49 +0800

    //策略改版備份 add file : apka_score_oscillate_11305281.py Trade_11305272.py
    //備份SQL查詢盈虧效能功能指令
    add file : 美股_TradeRecord_score_11305281.sql

---
+++ Trade_11305272.py
@@ -0,0 +1,168 @@
+#Trade
+import pandas as pd
+
+
+def TotProfit(xapka, Ticker):
+
+    def Xtred(i):
+        xtred = 0
+        for j in range(1,5):
+            xtred +=(xapka.at[i-j+1,"xmacd"] - xapka.at[i-j,"xmacd"])
+        return xtred    
+            
+    def ToBuy(i, xtred):
+        xema  = xapka.at[i,"xema"] 
+        yema  = xapka.at[i,"yema"] 
+        xmacd = xapka.at[i,"xmacd"]
+        OscSum= xapka.at[i,"OscSum"]
+        xTrnSlop  = xapka.at[i,"TrnSlop"]
+        prebuy  = xapka.at[i-1,"buy"]
+        if prebuy ==0:
+            if   (OscSum >=  6) or ( OscSum < 5 and xapka.at[i-1,"OscSum"] == 5 ) and xmacd <-1:
+                return 10
+            #elif ( OscSum < 4 and xapka.at[i-1,"OscSum"] == 4 ) and xmacd <-1:
+            #    return 11
+            #elif OscSum <= -5 and xmacd >=4 and xema >=4 and xTrnSlop<=0:
+            #    return -11            
+            elif  yema > xapka.at[i-1,"yema"]  and yema >=1 and xema >0 and xTrnSlop>=0:
+                return yema                          
+            elif yema < xapka.at[i-1,"yema"] and yema <=1 and OscSum <2 and xema <0  and xTrnSlop<0:
+                return yema
+            elif (xema >0 and xmacd >0 )and ( xapka.at[i-1,"xmacd"] <=0  or xapka.at[i-1,"xema"] <=0) and xTrnSlop>=0:
+                return 20
+            #elif (xema <0 and xmacd <0 )and ( xapka.at[i-1,"xmacd"] >=0  or xapka.at[i-1,"xema"] >=0) and xTrnSlop<0 :
+            #    return -20            
+        else:
+            return 0
+        
+    def ToSell(i, xtred):
+        xema   = xapka.at[i,"xema"] 
+        yema   = xapka.at[i,"yema"] 
+        xmacd  = xapka.at[i,"xmacd"]
+        OscSum = xapka.at[i,"OscSum"]
+        xTrnSlop  = xapka.at[i,"TrnSlop"]
+        xstate = xapka.at[i,"xstate"]
+        Prebuy = xapka.at[i-1,"buy"]
+        
+        if   Prebuy == 10 and (xmacd == 4 or  (OscSum<=-1 and xmacd <0) )  :
+            return Prebuy
+        elif Prebuy == 11 and OscSum == 0 :
+            return Prebuy
+        elif Prebuy == -11 and OscSum == 0 :
+            return Prebuy        
+        elif Prebuy < 0 and Prebuy > -10 and ( OscSum>=4 or yema > xapka.at[i-1,"yema"]) and xTrnSlop>=0:
+            return Prebuy
+        elif Prebuy > 0   and Prebuy < 10 and yema < xapka.at[i-1,"yema"] and xTrnSlop<0: #and Prebuy == xapka.at[i-1,"yema"]
+            return Prebuy
+        elif Prebuy ==20  and (OscSum <=-4 or (xmacd < xapka.at[i-1,"xmacd"] )) :     
+            return Prebuy
+        #elif Prebuy ==-20  and (OscSum >=4 or (xmacd > xapka.at[i-1,"xmacd"] )) :     
+        #    return Prebuy        
+        else:
+            return 0
+
+
+#---------------------------------------------------------------------------
+    def BbToBuy(i):
+        xBBand = xapka.at[i,"xBBand"]
+        prebuy  = xapka.at[i-1,"buy"]
+        
+        if prebuy ==0:
+            if   xBBand ==2:
+                return 1
+            #elif xBBand ==-2:
+            #    return -1               
+        else:
+            return 0
+        
+    def BbToSell(i):
+        xBBand  = xapka.at[i,"xBBand"]
+        Prebuy = xapka.at[i-1,"buy"]      
+        if   xBBand  <0 and Prebuy == 1:
+            return 1
+        elif xBBand  >0 and Prebuy == -1:
+            return -1
+        else:
+            return 0
+#---------------------------------------------------------------------------
+    def MacdToBuy(i):
+        xema   = xapka.at[i,"xema"] 
+        yema   = xapka.at[i,"yema"] 
+        xmacd  = xapka.at[i,"xmacd"]
+        Prebuy = xapka.at[i-1,"buy"] 
+        if Prebuy ==0:
+            if   xmacd > xapka.at[i-1,"xmacd"] and xapka.at[i-1,"xmacd"]== -1 :
+                return 1
+            #elif xBBand ==-2:
+            #    return -1               
+        else:
+            return 0
+        
+    def MacdToSell(i):
+        xema   = xapka.at[i,"xema"] 
+        yema   = xapka.at[i,"yema"] 
+        xmacd  = xapka.at[i,"xmacd"]
+        Prebuy = xapka.at[i-1,"buy"]   
+        if   xmacd < xapka.at[i-1,"xmacd"] and xapka.at[i-1,"xmacd"]== 2 and Prebuy == 1:
+            return 1
+        #elif xmacd < xapka.at[i-1,"xmacd"] and xapka.at[i-1,"xmacd"]== 2:
+        #    return -1
+        else:
+            return 0
+#---------------------------------------------------------------------------
+    buyPrice = 0.0
+    BuySellway =''
+    Buydate =''
+    LongShort =''
+
+    xapka.fillna(0)
+    #print(xapka.head())
+    TredRoc = pd.DataFrame()
+    for i in range(5,len(xapka)):
+        totred = int (Xtred(i) or 0)
+        toBuy = int (ToBuy(i, totred) or 0)
+        toSell= int (ToSell(i, totred) or 0) 
+        if toBuy >= 1 :
+            xapka.at[i,"buy"] =toBuy
+            buyPrice = xapka.at[i,"close"]
+            Buydate  = xapka.at[i, "date"]
+            BuySellway = BuySellway +'+Buy_'+str(xapka.at[i, "date"])[5:10]+' => ' 
+        elif toBuy <= -1 :
+            xapka.at[i,"buy"] =toBuy
+            buyPrice = xapka.at[i,"close"]
+            Buydate  = xapka.at[i, "date"]
+            BuySellway = BuySellway +'-Buy_'+str(xapka.at[i, "date"])[5:10]+' => '             
+        elif toSell >= 1 :
+            #xapka.at[i,"Sell"] =1
+            xapka.at[i,"buy"] =0
+            xapka.at[i,"profit"] = (xapka.at[i,"close"] - buyPrice)/buyPrice
+            #print(BuySellway +'Sell_'+str(xapka.at[i, "date"])[5:10]+'_profit= '+str( xapka.at[i,"profit"].round(3)))
+            a = [Ticker, xapka.at[i-1,"buy"], Buydate, xapka.at[i, "date"], round(buyPrice, 3), round(xapka.at[i,"close"], 3), round(xapka.at[i,"profit"],3)]
+            TredRoc = pd.concat([TredRoc, pd.DataFrame([a])], ignore_index=True)
+            BuySellway =''
+        elif toSell <= -1 :
+            #xapka.at[i,"Sell"] =-1
+            xapka.at[i,"buy"] =0
+            xapka.at[i,"profit"] = (buyPrice - xapka.at[i,"close"])/buyPrice
+            #print(BuySellway +'Sell_'+str(xapka.at[i, "date"])[5:10]+'_profit= '+str( xapka.at[i,"profit"].round(3)))
+            a = [Ticker, xapka.at[i-1,"buy"], Buydate, xapka.at[i, "date"], round(buyPrice, 3), round(xapka.at[i,"close"], 3), round(xapka.at[i,"profit"], 3)]
+            TredRoc = pd.concat([TredRoc, pd.DataFrame([a])], ignore_index=True)            
+            BuySellway =''
+        else:
+            #xapka.at[i,"Sell"] = xapka.at[i-1,"Sell"]
+            xapka.at[i,"buy"]  = xapka.at[i-1,"buy"]
+
+    EndDate = len(xapka)
+    init = total =100.0
+    count = 0
+    for i in range(1,EndDate):
+        if xapka.at[i,"profit"] != 0:
+            count = count +1
+            total = total + total*xapka.at[i,"profit"]
+    print( Ticker, "From [" + str(xapka.at[1,"date"]) +"] to ["+ str(xapka.at[len(xapka)-1,"date"]) +"] total =1000.0 after count: ["+str(count)+"] times total profit = "+str(round(total,3)) )
+    a = [Ticker+'_Total', 0, xapka.at[1,"date"], xapka.at[len(xapka)-1,"date"], init, count, round(total,3)]
+    TredRoc = pd.concat([TredRoc, pd.DataFrame([a])], ignore_index=True)
+    TredRoc.columns = ["Ticker","LongShort","Buydate","Selldate","buyPrice","SellPrice","profit"]
+    return TredRoc
+    #TredRoc.to_csv('TredRocord_'+Ticker+'.csv')     
+    
---
+++ apka_score_oscillate_11305281.py
@@ -0,0 +1,124 @@
+#apka_score_oscillate
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import Trade
+
+def score_oscillate(data):
+    xapka = pd.DataFrame()
+    for j in range(10,len(data)):
+        try:        
+            #===========================================================================================
+            if data.at[j, 'fastk_d'] > data.at[j, 'fastd_d'] and data.at[j, 'fastd_d'] < 20 :
+                xsrsi = 1
+                for k in range(j-10,j): 
+                    if  data.at[k, 'fastk_w'] < data.at[k, 'fastd_w'] and  data.at[k, 'fastd_w'] < 20 :
+                        xsrsi = 2
+            elif data.at[j, 'fastk_d'] < data.at[j, 'fastd_d'] and data.at[j, 'fastd_d'] > 90 :
+                xsrsi = -1
+                for k in range(j-10,j): 
+                    if data.at[k, 'fastk_w'] > data.at[k, 'fastd_w'] and  data.at[k, 'fastd_w'] > 90 :
+                        xsrsi = -2
+            else:
+                xsrsi = 0  
+            #===========================================================================================
+            if data.at[j, 'willrd'] < -95 : 
+                xwillrd = 1
+                if data.at[j, 'willrw'] <-95:
+                    xwillrd = 2
+            elif data.at[j, 'willrd'] > -5 :
+                xwillrd = -1
+                if  data.at[j, 'willrw'] >-5:
+                    xwillrd = -2
+            else:
+                xwillrd = 0
+            #===========================================================================================
+            #"upp_d","mid_d","low_d","upp_w","mid_w","low_w","upp_m","mid_m","low_m"
+            # (data.at[j, 'high'] - data.at[j, 'mid_d'])/(data.at[j, 'upp_d'] - data.at[j, 'mid_d']) > 0.9
+            LongShort = data.at[j, 'mid_d'] - data.at[j-1, 'mid_d']
+            BandWidth = (data.at[j, 'upp_d'] - data.at[j, 'low_d'])+1e-15
+            level     = (data.at[j, 'close'] - data.at[j, 'low_d'])/BandWidth
+
+            wBandWidth = (data.at[j, 'upp_w'] - data.at[j, 'low_w'])+1e-15
+            wlevel     = (data.at[j, 'close'] - data.at[j, 'low_w'])/wBandWidth
+            if level >0.8: 
+                xBBand = -1
+                if wlevel >0.8:
+                     xBBand = -2
+            elif level < 0.2:
+                xBBand = 1
+                if wlevel < 0.2:
+                    xBBand = 2
+            else:
+                xBBand = 0
+            '''
+            if  data.at[j, 'low'] < data.at[j, 'low_d'] and data.at[j-1, 'low'] > data.at[j-1, 'low_d']: 
+                xBBand = 1
+                if data.at[j, 'low'] < data.at[j, 'low_w'] :
+                    xBBand = 2   
+            if  data.at[j, 'high'] > data.at[j, 'upp_d'] and data.at[j-1, 'close'] < data.at[j-1, 'upp_d']: 
+                xBBand = -1
+                if data.at[j, 'high'] > data.at[j, 'upp_w'] :
+                     xBBand = -2
+            else:
+                xBBand = 0
+
+            '''
+            sum= xBBand+xsrsi+xwillrd    
+            #===========================================================================================
+            a=[ data.at[j,'date'],data.at[j,'close'], xsrsi, xwillrd,xBBand, sum, level, 0, 0, 0.0]
+            xapka = pd.concat([xapka, pd.DataFrame([a])], ignore_index=True)                
+        except Exception as errMsg: 
+            print('每K線計算發生錯誤-', str(data.at[j,'date']) , errMsg)                   
+
+    xapka.columns = ["date", "close", "xsrsi", "xwillrd", "xBBand", "OscSum", "BBlevel","buy", "sell", "profit"]
+    return xapka
+    '''
+    fig = plt.figure(num =1, figsize=(18,9))    #創建圖表
+    sub1 = fig.add_subplot(2, 1, 1) # 添加子圖表1
+    sub2 = fig.add_subplot(2, 1, 2) # 添加子圖表2
+    sub1.plot(xapka["date"],xapka["close"],label="close" ,color = 'blue')        
+    sub2.plot(xapka["date"],xapka["sum"], label="sum" , linewidth = 1, linestyle = '-' ,color = 'red')
+    sub2.plot(xapka["date"],xapka["xsrsi"],label="xsrsi" , linewidth = 1, linestyle = '-.',color = 'orange') 
+    sub2.plot(xapka["date"],xapka["xwillrd"],label="xwillrd" , linewidth = 1, linestyle = '--',color = 'green')
+    sub2.plot(xapka["date"],xapka["xBBand"],label="xBBand" , linewidth = 1, linestyle = '-.',color = 'blue')
+    #決策進出場
+    for i in range(1,len(xapka)):
+        if xapka.at[i, "sum"] >=5 :
+            sub1.text(xapka.at[i, "date"], xapka.at[i, "close"],str(xapka.at[i, "date"])[5:10],rotation=90,color='blue')
+            sub2.text(xapka.at[i, "date"], xapka.at[i, "sum"],str(xapka.at[i, "date"])[5:10],rotation=90,color='blue')
+        elif xapka.at[i, "sum"] <=-5:
+            sub1.text(xapka.at[i, "date"],xapka.at[i, "close"],str(xapka.at[i, "date"])[5:10],rotation=90,color='red')
+            sub2.text(xapka.at[i, "date"], xapka.at[i, "sum"],str(xapka.at[i, "date"])[5:10],rotation=90,color='red') 
+    
+    buy = 0.0
+    BuySellway =''
+    for i in range(1,len(xapka)):
+        if xapka.at[i,"xsrsi"] + xapka.at[i,"xsrsi"] + xapka.at[i,"xsrsi"] >3 :
+            sub2.text(xapka.at[i, "date"], 1,str(xapka.at[i, "date"])[5:10],color='green')
+            xapka.at[i,"buy"] =1
+            buy = xapka.at[i,"close"]
+            BuySellway = BuySellway +'Buy_'+str(xapka.at[i, "date"])[5:10]+'=>' 
+        elif xapka.at[i,"xmacd"] <0 and xapka.at[i-1,"xmacd"]>=0:
+            sub2.text(xapka.at[i, "date"],-1,str(xapka.at[i, "date"])[5:10],color='black') 
+            xapka.at[i,"Sell"] =1
+            xapka.at[i,"profit"] = (xapka.at[i,"close"] - buy)/buy
+            #print(BuySellway +'Sell_'+str(xapka.at[i, "date"])[5:10]+'_profit= '+str(xapka.at[i,"profit"].round(3)))
+            BuySellway ='' 
+    total =1000.0
+    count = 0
+    EndDate = len(xapka)
+    for i in range(1,len(xapka)):
+        if xapka.at[i,"profit"] != 0:
+            count = count +1
+            total = total + total*xapka.at[i,"profit"]
+    print("From [" + str(xapka.at[1,"date"]) +"] to ["+ str(xapka.at[len(xapka)-1,"date"]) +"] total =1000.0 after count: []"+str(count)+"] times total profit = "+str(total) )
+    
+    
+    
+    #Trade.TotProfit(xapka)
+
+    plt.subplots_adjust(left=0.05,bottom=0.07,right=0.97,top=0.97,wspace=0.12,hspace=0.12)
+    plt.legend() 
+    plt.show()
+    '''
