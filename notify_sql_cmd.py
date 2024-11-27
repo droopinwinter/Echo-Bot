4c7c1066c47e6529c74e779b69e478081abde7b4
tom.gau <droopinwinter@gmail.com>
2024-11-27 11:39:24 +0800

    增加追蹤檔案 add file : analsy_score_day_append_HomePC.py notify_sql_cmd.py symbol2Chtext.py test.py

---
+++ notify_sql_cmd.py
@@ -0,0 +1,130 @@
+# notify_sql_cmd
+
+def DelDupDay( xTicker):
+    return  "DELETE T FROM ("+\
+            "	SELECT *, DupRank = ROW_NUMBER() OVER (PARTITION BY [date], [xremark] ORDER BY (SELECT NULL))"+\
+            f"	FROM [apka].[dbo].[ApkaDay_Combin]) AS T WHERE DupRank > 1"
+
+def DelLastDay( xTicker, SqlMaxDate):
+    return f"DELETE [analsy].[dbo].[ApkaDay_Combin] Where Date >= '{SqlMaxDate} 00:00:00.000'"
+#=================================================
+# 買賣徵兆
+def Sig2BuySell( befDate, countyr):
+    if countyr == 'TW':
+        countyr = "and right([xremark], 3) = '.TW'"
+        mark    = "substring([xremark],4,10)"
+        dateR   = f"between GETDATE()-1 and  GETDATE()"     
+    else:
+        countyr = "and right([xremark], 3) <> '.TW'"  
+        mark    = "substring([xremark],4,5)"  
+        dateR   = f"between GETDATE()-{befDate} and  GETDATE()-1"  
+    return f"SELECT distinct [date],[buySig] buy,[SellSig] Sell,[OscSum] Osc,[BBlevel] BB,[TrnSlop] slp ,{mark} typ"+\
+    " FROM [apka].[dbo].[ApkaDay_Combin] "+\
+    f" where date {dateR} AND ( buySig <>0 or SellSig <> 0) {countyr} order by date,typ "
+
+#=================================================
+# 接近布林帶下沿
+def Sig2LowBull( befDate, countyr):
+    if countyr == 'TW':
+        countyr = "and right([xremark], 3) = '.TW'"
+        mark    = "substring([xremark],4,10)"
+        dateR   = f"between GETDATE()-1 and  GETDATE()"       
+    else:
+        countyr = "and right([xremark], 3) <> '.TW'"      
+        mark    = "substring([xremark],4,5)"
+        dateR   = f"between GETDATE()-{befDate} and  GETDATE()-1"  
+    return f"SELECT distinct [date],[BBlevel] BB,[OscSum] Osc,[TrnSlop] slp ,{mark} typ "+\
+       " FROM [apka].[dbo].[ApkaDay_Combin] "+\
+       f" where date {dateR} AND ([BBlevel]<0.2 ) {countyr} order by date,typ "
+
+#=================================================
+# 接近布林帶上沿
+def Sig2HighBull( befDate, countyr):
+    if countyr == 'TW':
+        countyr = "and right([xremark], 3) = '.TW'"
+        mark    = "substring([xremark],4,10)"
+        dateR   = f"between GETDATE()-1 and  GETDATE()"   
+    else:
+        countyr = "and right([xremark], 3) <> '.TW'"  
+        mark    = "substring([xremark],4,5)"
+        dateR   = f"between GETDATE()-{befDate} and  GETDATE()-1"
+    return f"SELECT distinct [date],[BBlevel] BB,[OscSum] Osc,[TrnSlop] slp ,{mark} typ "+\
+       " FROM [apka].[dbo].[ApkaDay_Combin]  "+\
+       f" where date {dateR} AND ([BBlevel]>0.9 ) {countyr} order by date,typ"
+#=================================================
+# 當日收盤後技術分析擺盪
+def Sig2Osc( befDate, countyr):
+    if countyr == 'TW':
+        countyr = "and right([xremark], 3) = '.TW'"
+        mark    = "substring([xremark],4,10)"
+        dateR   = f"between GETDATE()-1 and  GETDATE()" 
+    else:
+        countyr = "and right([xremark], 3) <> '.TW'"  
+        mark    = "substring([xremark],4,5)"
+        dateR   = f"between GETDATE()-{befDate} and  GETDATE()-1"
+    return f"SELECT  distinct [date],[buy],[OscSum] Osc,[BBlevel] BB, {mark} typ "+\
+       " FROM [apka].[dbo].[ApkaDay_Combin] "+\
+       f" where date {dateR}  {countyr} "+\
+       " order by date,xremark"
+#=================================================
+# 當日收盤後技術分析趨勢＿股指前11個
+#Sql5 = "SELECT  distinct [date],[xema] ema,[xmacd] mcd,[TrnSlop] slp,[OscSum] Os,[BBlevel] BB,[xremark] typ "+\
+#       " FROM [apka].[dbo].[ApkaDay_Combin] "+\
+#       " where date > GETDATE()-2 "+\
+#       " order by date,xremark"
+def Sig2TrendAndOsc_part1( befDate, countyr):
+    if countyr == 'TW':
+        countyr = "and right([xremark], 3) = '.TW'"
+        mark    = "substring([xremark],4,10)"
+        #range   = "'01' and '80'"
+        range   = " '01' and '22'"
+        dateR   = f"between GETDATE()-1 and  GETDATE()"
+    else:
+        countyr = "and right([xremark], 3) <> '.TW'"
+        mark    = "substring([xremark],4,5)"
+        range   = "'01' and '80'"
+        range   = "'01' and '22'"
+        dateR   = f"between GETDATE()-{befDate} and  GETDATE()-1"
+    return "SELECT [date],[buy] bi,[xmacd] md,[TrnSlop] sp,[OscSum] Os,[BBlevel] BB,[mark] typ FROM ( "+\
+      "  SELECT distinct [date],[xema],[xmacd],[TrnSlop],[buySig],[SellSig],[buy],[OscSum] "+\
+      f"  ,[BBlevel], substring([xremark],1,2) pre, {mark} mark "+\
+      f"  FROM [apka].[dbo].[ApkaDay_Combin] where date {dateR} {countyr} "+\
+      f" and left([xremark], 2) between {range} ) a order by pre "
+#=================================================
+# 當日收盤後技術分析趨勢＿板塊11個
+def Sig2TrendAndOsc_part2( befDate, countyr):
+    
+    if countyr == 'TW':
+        countyr = "and right([xremark], 3) = '.TW'"
+        mark    = "substring([xremark],4,10)"
+        range   = "'23' and '44'"  
+        dateR   = f"between GETDATE()-1 and  GETDATE()"
+    else:
+        countyr = "and right([xremark], 3) <> '.TW'"  
+        mark    = "substring([xremark],4,5)"
+        range   = "'23' and '44'"
+        dateR   = f"between GETDATE()-{befDate} and  GETDATE()-1"
+    return "SELECT [date],[buy] bi,[xmacd] md,[TrnSlop] sp,[OscSum] Os,[BBlevel] BB,[mark] typ FROM ( "+\
+      "  SELECT distinct [date],[xema],[xmacd],[TrnSlop],[buySig],[SellSig],[buy],[OscSum] "+\
+      f"  ,[BBlevel], substring([xremark],1,2) pre, {mark} mark "+\
+      f"  FROM [apka].[dbo].[ApkaDay_Combin] where date  {dateR} {countyr} "+\
+      f" and left([xremark], 2) between {range} ) a order by pre "
+
+#=================================================
+# 當日收盤後技術分析趨勢＿大科技＿11個
+def Sig2TrendAndOsc_part3( befDate, countyr):
+    if countyr == 'TW':
+        countyr = "and right([xremark], 3) = '.TW'"
+        mark    = "substring([xremark],4,10)"
+        range   = "'44' and '60'"
+        dateR   = f"between GETDATE()-1 and  GETDATE()"
+    else:
+        countyr = "and right([xremark], 3) <> '.TW'"
+        mark    = "substring([xremark],4,5)"  
+        range   = "'22' and '40'"
+        dateR   = f"between GETDATE()-{befDate} and  GETDATE()-1"
+    return "SELECT [date],[buy] bi,[xmacd] md,[TrnSlop] sp,[OscSum] Os,[BBlevel] BB,[mark] typ FROM ( "+\
+      "  SELECT distinct [date],[xema],[xmacd],[TrnSlop],[buySig],[SellSig],[buy],[OscSum] "+\
+      f"  ,[BBlevel], substring([xremark],1,2) pre, {mark} mark "+\
+      f"  FROM [apka].[dbo].[ApkaDay_Combin] where date  {dateR}  {countyr} "+\
+      f" and left([xremark], 2) between {range} ) a order by pre "
---
+++ symbol2Chtext.py
@@ -0,0 +1,139 @@
+#symbol2Chtext
+import pandas as pd
+
+def TWsymbol2Chtext(xSymbol):
+    match xSymbol:
+        #case "": return "JavaScript"
+        case "2330.TW":    return "台積電"
+        case "2317.TW":    return "鴻海"
+        case "2454.TW":    return "聯發科"
+        case "2881.TW":    return "富邦金"
+        case "2382.TW":    return "廣達"
+        case "2882.TW":    return "國泰金"
+        case "2412.TW":    return "中華電"
+        case "2308.TW":    return "台達電"
+        case "2891.TW":    return "中信金"
+        case "3711.TW":    return "日月光投控"
+        case "2303.TW":    return "聯電"
+        case "2886.TW":    return "兆豐金"
+        case "6505.TW":    return "台塑化"
+        case "1216.TW":    return "統一"
+        case "2884.TW":    return "玉山金"
+        case "3045.TW":    return "台灣大"
+        case "2885.TW":    return "元大金"
+        case "0050.TW":    return "元大台灣50"
+        case "2603.TW":    return "長榮"
+        case "00632R.TW":  return "元大台灣50反1"
+        case "00676R.TW":  return "富邦臺灣加權反1"
+        case "00674R.TW":  return "期元大S&P黃金反1"
+        case "00707R.TW":  return "期元大S&P日圓反1"
+        case "^TWII":      return "台灣加權指數"
+        case "^N225":      return "日經指數"
+        case "^KS11":      return "韓國指數"
+        case "006208.TW":  return "富邦台50"
+        case "2357.TW":    return "華碩"
+        case "00631L.TW":  return "元大台灣50正2"
+        case "00642U.TW":  return "期元大S&P石油"
+        case "00645.TW":   return "富邦日本"
+        case "00661.TW":   return "元大日經225"
+        case "00685L.TW":  return "群益臺灣加權正2"
+        case "00738U.TW":  return "期元大道瓊白銀"
+        case "00708L.TW":  return "期元大S&P黃金正2"
+        case "00640L.TW":  return "富邦日本正2"
+        case "00635U.TW":  return "期元大S&P黃金"
+        case "00693U.TW":  return "期街口S&P黃豆"
+        case "00763U.TW":  return "期街口道瓊銅"
+        case "00683L.TW":  return "期元大美元指正2"
+        case "00663L.TW":  return "國泰臺灣加權正2"
+        case "00709.TW" :  return "富邦歐洲"
+        case "00660.TW" :  return "元大歐洲50"
+        case "00682U.TW":  return "期元大美元指數" 
+               
+def USdf2Chtext(df):
+    df1=df.replace({"typ":{
+                        '2330.TW':   '股_台積電'
+                        ,'2317.TW':  '股_鴻海'
+                        ,'2454.TW':  '股_聯發科'
+                        ,'2881.TW':  '股_富邦金'
+                        ,'2382.TW':  '股_廣達'
+                        ,'2882.TW':  '股_國泰金'
+                        ,'2412.TW':  '股_中華電'
+                        ,'2308.TW':  '股_台達電'
+                        ,'2891.TW':  '股_中信金'
+                        ,'3711.TW':  '股_日月光投控'
+                        ,'2303.TW':  '股_聯電'
+                        ,'2886.TW':  '股_兆豐金'
+                        ,'6505.TW':  '股_台塑化'
+                        ,'1216.TW':  '股_統一'
+                        ,'2884.TW':  '股_玉山金'
+                        ,'3045.TW':  '股_台灣大'
+                        ,'2885.TW':  '股_元大金'
+                        ,'0050.TW':  '股_元大台灣50'
+                        ,'2603.TW':  '股_長榮'
+                        ,'2357.TW':  '股_華碩'
+                        ,'^TWII':    '指_台灣加權指數'
+                        ,'^N225':    '指_日經指數'
+                        ,'^KS11':    '指_韓國指數'
+                        ,'00642U.TW':'期_元大S&P石油'
+                        ,'00674R.TW':'期_元大S&P黃金反1'
+                        ,'00707R.TW':'期_元大S&P日圓反1'
+                        ,'00738U.TW':'期_元大道瓊白銀'
+                        ,'00708L.TW':'期_元大S&P黃金正2'
+                        ,'00635U.TW':'期_元大S&P黃金'
+                        ,'00693U.TW':'期_街口S&P黃豆'
+                        ,'00763U.TW':'期_街口道瓊銅'
+                        ,'00683L.TW':'期_元大美元指正2'
+                        ,'00682U.TW':'期_元大美元指數'
+                        ,'00645.TW': 'ETF_富邦日本'
+                        ,'00661.TW': 'ETF_元大日經225'
+                        ,'00685L.TW':'ETF_群益臺灣加權正2'
+                        ,'006208.TW':'ETF_富邦台50'
+                        ,'00631L.TW':'ETF_元大台灣50正2'
+                        ,'00632R.TW':'ETF_元大台灣50反1'
+                        ,'00676R.TW':'ETF_富邦臺灣加權反1'
+                        ,'00663L.TW':'ETF_國泰臺灣加權正2'
+                        ,'00709.TW': 'ETF_富邦歐洲'
+                        ,'00660.TW': 'ETF_元大歐洲50'
+                        ,'00640L.TW':'ETF_富邦日本正2'
+                        ,'SPY':  '指_標普500指'
+                        ,'QQQ':  '指_納斯達克100指'
+                        ,'DIA':  '指_道瓊斯30指'
+                        ,'IWM':  '指_羅素小盤股指'
+                        ,'SOXX': '指_費城半導體指'
+                        ,'CL':   '期_WTI原油期貨'
+                        ,'TLT':  '期_20年美債ETF正'
+                        ,'TMV':  '期_20年美債ETF反3'
+                        ,'^TWII':'指_台灣加權指'
+                        ,'^N225':'指_日經255指'
+                        ,'^KS11':'指_南韓綜合指'
+                        ,'FNGU': 'ETF_大科技正3'
+                        ,'SOXL': 'ETF_費半正3'
+                        ,'XBI':  'ETF_生物科技'
+                        ,'XLE':  'ETF_能源股'
+                        ,'XLF':  'ETF_金融股'
+                        ,'XME':  'ETF_礦業金屬股'
+                        ,'XLV':  'ETF_醫療保健股'
+                        ,'XLP':  'ETF_日常消費'
+                        ,'IYR':  'ETF_房地產股'
+                        ,'XLU':  'ETF_公用事業'
+                        ,'XLI':  'ETF_工業股'
+                        ,'TSLA': '股_特斯拉'
+                        ,'AAPL': '股_蘋果'
+                        ,'NVDA': '股_英偉達'
+                        ,'MSFT': '股_微軟'
+                        ,'AMZN': '股_亞馬遜'
+                        ,'NFLX': '股_奈飛'
+                        ,'META': '股_臉書'
+                        ,'AMD':  '股_美國超微'
+                        ,'GOOGL':'股_谷哥'
+                        ,'INTC': '股_英特爾'
+                        ,'MU':   '股_美光'
+                        ,'TSM':  '股_台積電ADR'
+                          }})
+    return df1
+
+def df2Chtext(df):
+    df['bi'].replace(['[-20--1','持空單'],regex=True,inplace=True)
+    df['bi'].replace(['[20-1'  ,'持多單'],regex=True,inplace=True)
+    df['bi'].replace(['[0'     ,'觀望中'],regex=True,inplace=True)
+    return df
