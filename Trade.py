d1eeed7f4c279711bff47fc1895b65fdf69f9605
tom.gau <droopinwinter@gmail.com>
2025-01-08 17:10:28 +0800

    //交易規則判斷增加EFI指標與神奇九轉指標 update file : Trade.py         xema    = xapka.at[i,"xema"]         xema_1  = xapka.at[i-1,"xema"]         yema    = xapka.at[i,"yema"]         yema_1  = xapka.at[i-1,"yema"]         xmacd   = xapka.at[i,"xmacd"]         xmacd_1 = xapka.at[i-1,"xmacd"]         OscSum  = xapka.at[i,"OscSum"]         OscSum_1= xapka.at[i-1,"OscSum"]         xTrnSlop= xapka.at[i,"TrnSlop"]         prebuy  = xapka.at[i-1,"buy"]         xBBand  = xapka.at[i,"xBBand"]         xplse   = xapka.at[i,"plse"]         xplse_1 = xapka.at[i-1,"plse"]         cnt9    = xapka.at[i-1,"count9"] ###################################################### add file : Trade_11401071.py

--- Trade.py
+++ Trade.py
@@ -13,45 +13,62 @@ def TotProfit(xapka, Ticker, CurrDateTime, rzt):
         return xtred    
             
     def ToBuy(i, xtred):
-        xema  = xapka.at[i,"xema"] 
-        yema  = xapka.at[i,"yema"] 
-        xmacd = xapka.at[i,"xmacd"]
-        OscSum= xapka.at[i,"OscSum"]
-        xTrnSlop  = xapka.at[i,"TrnSlop"]
+        xema    = xapka.at[i,"xema"]
+        xema_1  = xapka.at[i-1,"xema"]
+        yema    = xapka.at[i,"yema"] 
+        yema_1  = xapka.at[i-1,"yema"] 
+        xmacd   = xapka.at[i,"xmacd"]
+        xmacd_1 = xapka.at[i-1,"xmacd"]
+        OscSum  = xapka.at[i,"OscSum"]
+        OscSum_1= xapka.at[i-1,"OscSum"]
+        xTrnSlop= xapka.at[i,"TrnSlop"]
         prebuy  = xapka.at[i-1,"buy"]
+        xBBand  = xapka.at[i,"xBBand"]
+        xplse   = xapka.at[i,"plse"]
+        xplse_1 = xapka.at[i-1,"plse"]
+        cnt9    = xapka.at[i-1,"count9"]
+
         if prebuy ==0:
-            if  ( OscSum==8 or (OscSum <  6 and xapka.at[i-1,"OscSum"] == 6 ) \
-                 or ( OscSum < 4 and xapka.at[i-1,"OscSum"] == 4 ) )and (xmacd <-2 or xema<-2) :
+            if  ( OscSum==8 or (OscSum <  6 and OscSum_1 == 6 ) \
+                 or ( OscSum < 4 and OscSum_1 == 4 ) )and (xmacd <-2 or xema<-2) :
                 xapka.at[i,"buySig"] =10
                 return 10 #+極端交易
-            #elif ( OscSum < 4 and xapka.at[i-1,"OscSum"] == 4 ) and xmacd <=-1:
+            #elif ( OscSum < 4 and OscSum_1 == 4 ) and xmacd <=-1:
             #    return 11 #+極端交易
             #elif OscSum <= -6 and xmacd >=4 and xema >=4 and xTrnSlop<=0:
             #    return -11            
-            elif  yema > xapka.at[i-1,"yema"]  and yema >0 :#and xTrnSlop>0
+            elif  yema > yema_1  and yema >0 :#and xTrnSlop>0
                 xapka.at[i,"buySig"] =yema
                 return yema  #+GMMA交易                        
-            #elif yema < xapka.at[i-1,"yema"] and yema <=-1 and xema <0 and xTrnSlop<=0:
+            #elif yema < yema_1 and yema <=-1 and xema <0 and xTrnSlop<=0:
             #    xapka.at[i,"buySig"] =yema
             #    return yema #-GMMA交易
-            elif xmacd >0 and xapka.at[i-1,"xmacd"] <=0   and xTrnSlop>0:
+            elif xmacd >0 and xmacd_1 <=0   and xTrnSlop>0:
                 xapka.at[i,"buySig"] =20
                 return 20 #+趨勢交易
-            elif  xmacd <0 and xapka.at[i-1,"xmacd"] >=0 and xTrnSlop<-0.1:
+            elif  xmacd <0 and xmacd_1 >=0 and xTrnSlop<-0.1:
                 xapka.at[i,"buySig"] =-20
                 return -20            
         else:
             return 0
         
     def ToSell(i, xtred):
-        xema   = xapka.at[i,"xema"] 
-        yema   = xapka.at[i,"yema"] 
-        xmacd  = xapka.at[i,"xmacd"]
-        OscSum = xapka.at[i,"OscSum"]
-        xTrnSlop  = xapka.at[i,"TrnSlop"]
-        xstate = xapka.at[i,"xstate"]
+        xema    = xapka.at[i,"xema"]
+        xema_1  = xapka.at[i-1,"xema"]
+        yema    = xapka.at[i,"yema"] 
+        yema_1  = xapka.at[i-1,"yema"] 
+        xmacd   = xapka.at[i,"xmacd"]
+        xmacd_1 = xapka.at[i-1,"xmacd"]
+        OscSum  = xapka.at[i,"OscSum"]
+        OscSum_1= xapka.at[i-1,"OscSum"]
+        xTrnSlop= xapka.at[i,"TrnSlop"]
+        prebuy  = xapka.at[i-1,"buy"]
+        xBBand  = xapka.at[i,"xBBand"]
+        xplse   = xapka.at[i,"plse"]
+        xplse_1 = xapka.at[i-1,"plse"]
+        cnt9    = xapka.at[i-1,"count9"]
+        
         Prebuy = xapka.at[i-1,"buy"]
-        xlevel = xapka.at[i-1,"BBlevel"]
         if   Prebuy == 10 and (xmacd == 4 or (OscSum<=-1 and xmacd <0) )  :
             xapka.at[i,"SellSig"] =110  
             return Prebuy #+極端交易
@@ -61,76 +78,27 @@ def TotProfit(xapka, Ticker, CurrDateTime, rzt):
         elif Prebuy == -11 and OscSum == 0 :
             xapka.at[i,"SellSig"] =-110
             return Prebuy  #-極端交易      
-        elif Prebuy < 0 and Prebuy > -10 and ( yema > xapka.at[i-1,"yema"] or xmacd > xapka.at[i-1,"xmacd"]):
+        elif Prebuy < 0 and Prebuy > -10 and ( yema > yema_1 or xmacd > xmacd_1):
             xapka.at[i,"SellSig"] =Prebuy-100
             return Prebuy #-GMMA交易
-        elif Prebuy > 0   and Prebuy < 10 and(yema < xapka.at[i-1,"yema"] or xmacd < xapka.at[i-1,"xmacd"] ): #and Prebuy == xapka.at[i-1,"yema"]
+        elif Prebuy > 0   and Prebuy < 10 and(yema < yema_1 or xmacd < xmacd_1 ): #and Prebuy == yema_1
             xapka.at[i,"SellSig"] =Prebuy+100
             return Prebuy #+GMMA交易
-        elif Prebuy ==20  and (OscSum <=-6 or (xmacd < xapka.at[i-1,"xmacd"] ) \
-                               or (xema < xapka.at[i-1,"xema"] ) ) :     
+        elif Prebuy ==20  and (OscSum <=-6 or (xmacd < xmacd_1 ) \
+                               or (xema < xema_1 ) ) :     
             xapka.at[i,"SellSig"] =Prebuy+100
             return Prebuy #+趨勢交易
-        elif Prebuy ==-20  and ( OscSum >=3 or (xmacd > xapka.at[i-1,"xmacd"] and  xmacd<=0 ) or (xema > xapka.at[i-1,"xema"] and  xema <=0))  :     
+        elif Prebuy ==-20  and ( OscSum >=3 or (xmacd > xmacd_1 and  xmacd<=0 ) or (xema > xema_1 and  xema <=0))  :     
             xapka.at[i,"SellSig"] =Prebuy-100
             return Prebuy        
         else:
             return 0
 
-
-#---------------------------------------------------------------------------
-    def BbToBuy(i):
-        xBBand = xapka.at[i,"xBBand"]
-        prebuy  = xapka.at[i-1,"buy"]
-        
-        if prebuy ==0:
-            if   xBBand ==2:
-                return 1
-            #elif xBBand ==-2:
-            #    return -1               
-        else:
-            return 0
-        
-    def BbToSell(i):
-        xBBand  = xapka.at[i,"xBBand"]
-        Prebuy = xapka.at[i-1,"buy"]      
-        if   xBBand  <0 and Prebuy == 1:
-            return 1
-        elif xBBand  >0 and Prebuy == -1:
-            return -1
-        else:
-            return 0
-#---------------------------------------------------------------------------
-    def MacdToBuy(i):
-        xema   = xapka.at[i,"xema"] 
-        yema   = xapka.at[i,"yema"] 
-        xmacd  = xapka.at[i,"xmacd"]
-        Prebuy = xapka.at[i-1,"buy"] 
-        if Prebuy ==0:
-            if   xmacd > xapka.at[i-1,"xmacd"] and xapka.at[i-1,"xmacd"]== -1 :
-                return 1
-            #elif xBBand ==-2:
-            #    return -1               
-        else:
-            return 0
-        
-    def MacdToSell(i):
-        xema   = xapka.at[i,"xema"] 
-        yema   = xapka.at[i,"yema"] 
-        xmacd  = xapka.at[i,"xmacd"]
-        Prebuy = xapka.at[i-1,"buy"]   
-        if   xmacd < xapka.at[i-1,"xmacd"] and xapka.at[i-1,"xmacd"]== 2 and Prebuy == 1:
-            return 1
-        #elif xmacd < xapka.at[i-1,"xmacd"] and xapka.at[i-1,"xmacd"]== 2:
-        #    return -1
-        else:
-            return 0
-#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------    
     buyPrice = 0.0
     BuySellway =''
     Buydate =''
     LongShort =''
-
     xapka.fillna(0)
     #print(xapka.head())
     TredRoc = pd.DataFrame()
@@ -179,7 +147,7 @@ def TotProfit(xapka, Ticker, CurrDateTime, rzt):
     print( Ticker, "From [" + str(xapka.at[1,"date"]) +"] to ["+ str(xapka.at[len(xapka)-1,"date"]) +"] total =1000.0 after count: ["+str(count)+"] times total profit = "+str(round(total,3)) )
     
 
-    mtime = os.path.getmtime('D:\Stock_bk\Trade.py') #修改时间
+    mtime = os.path.getmtime('D:\\Stock_bk\\Trade.py') #修改时间
     mtime_string = datetime.fromtimestamp(int(mtime))
     b = [Ticker, mtime_string, xapka.at[1,"date"], xapka.at[len(xapka)-1,"date"], init, count, round(total,3),CurrDateTime]
     TredProfit = pd.concat([TredProfit, pd.DataFrame([b])], ignore_index=True)
@@ -199,9 +167,6 @@ def TotProfit(xapka, Ticker, CurrDateTime, rzt):
             #print(SingTredRoc)
         except Exception as errMsg:# 如果 try 的內容發生錯誤，就執行 except 裡的內容
             print('回存Trade資料庫錯誤_', Ticker , errMsg) 
-        
-    
-
 
     if rzt == 1:
         return xapka
@@ -209,3 +174,50 @@ def TotProfit(xapka, Ticker, CurrDateTime, rzt):
         return TredRoc
     #TredRoc.to_csv('TredRocord_'+Ticker+'.csv')     
     
+    #---------------------------------------------------------------------------
+    def BbToBuy(i):
+        xBBand = xapka.at[i,"xBBand"]
+        prebuy  = xapka.at[i-1,"buy"]
+        
+        if prebuy ==0:
+            if   xBBand ==2:
+                return 1
+            #elif xBBand ==-2:
+            #    return -1               
+        else:
+            return 0
+        
+    def BbToSell(i):
+        xBBand  = xapka.at[i,"xBBand"]
+        Prebuy = xapka.at[i-1,"buy"]      
+        if   xBBand  <0 and Prebuy == 1:
+            return 1
+        elif xBBand  >0 and Prebuy == -1:
+            return -1
+        else:
+            return 0
+#---------------------------------------------------------------------------
+    def MacdToBuy(i):
+        xema   = xapka.at[i,"xema"] 
+        yema   = xapka.at[i,"yema"] 
+        xmacd  = xapka.at[i,"xmacd"]
+        Prebuy = xapka.at[i-1,"buy"] 
+        if Prebuy ==0:
+            if   xmacd > xmacd_1 and xmacd_1== -1 :
+                return 1
+            #elif xBBand ==-2:
+            #    return -1               
+        else:
+            return 0
+        
+    def MacdToSell(i):
+        xema   = xapka.at[i,"xema"] 
+        yema   = xapka.at[i,"yema"] 
+        xmacd  = xapka.at[i,"xmacd"]
+        Prebuy = xapka.at[i-1,"buy"]   
+        if   xmacd < xmacd_1 and xmacd_1== 2 and Prebuy == 1:
+            return 1
+        #elif xmacd < xmacd_1 and xmacd_1== 2:
+        #    return -1
+        else:
+            return 0
